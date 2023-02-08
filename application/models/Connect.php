415e40061aa433819de1bae7073a1d0bf6ff760a
MamyAndry <119005054+MamyAndry@users.noreply.github.com>
2023-02-08 13:10:13 +0300

    ajout controller et modification de model
    controller:
     -formLogin
    model:
     -connect

--- application/models/Connect.php
+++ application/models/Connect.php
@@ -4,10 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Connect extends CI_Model {
     
     public function connectUser($mail,$mdp){
-        $requete = "SELECT * FROM utilisateur WHERE email = '%s' AND password = '%s' AND estAdmin = 0";
+        $requete = "SELECT * FROM utilisateur WHERE email = '%s' AND password = '%s'";
         $requete = sprintf($requete,$mail,$mdp);
         $query = $this->db->query($requete);
         $result = $query->row_array();
+        $this->session->set_userData("idUser",$result['iduser']);
+        return $result;
     }
 
     public function connectAdmin($mail,$mdp){
@@ -15,7 +17,18 @@ class Connect extends CI_Model {
         $requete = sprintf($requete,$mail,$mdp);
         $query = $this->db->query($requete);
         $result = $query->row_array();
+        $this->session->set_userData("id",$result['iduser']);
+        return $result;
     }
 
-    
+    public function isAdmin($id){
+        $requete = "SELECT idUser FROM utilisateur WHERE id = %s estAdmin = 1";
+        $requete = sprintf($requete,$id);
+        $query = $this->db->query($requete);
+        $result = $query->row_array();
+        if($res != null){
+            return true;
+        }
+        return false;
+    }
 }
