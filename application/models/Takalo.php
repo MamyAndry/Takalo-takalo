595428e7d61603d38df64e5612cc6a8a01931bca
MamyAndry <119005054+MamyAndry@users.noreply.github.com>
2023-02-08 21:23:30 +0300

    Update Takalo.php

--- application/models/Takalo.php
+++ application/models/Takalo.php
@@ -1,14 +1,28 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Takalo extends CI_Model { 
-    public function ($idUtilisateur){
-        $requete = "SELECT * FROM objet WHERE idobjet = %i";
-        $requete = sprintf($requete,$idUtilisateur);
-        $query = $this->db->query($requete);
-        $result = $query->row_array();
-        $this->session->set_userData("id",$result['idUser']);
-        return $result;
+class Takalo extends CI_Model{ 
+    
+    public function confirmer($idechange){
+        $sql = "UPDATE echange SET etatechange = 1 WHERE idEchange = %i";
+        $sql = sprintf($sql,$idechange);
+        $query = $this->db->query($sql);
+        $result = $query->affected_row();
+    }
+
+    public function refuser($idechange){
+        $sql = "UPDATE echange SET etatechange = -1 WHERE idEchange = %i";
+        $sql = sprintf($sql,$idechange);
+        $query = $this->db->query($sql);
+        $result = $query->affected_row();
+    }
+
+
+    public function atakalo($idUser1,$idUser2,$idobjet1,$idobjet2){
+        $sql = "INSERT INTO echange VALUES(null,%i,%i,%i,%i,0)";
+        $sql = sprintf($sql,$idUser1,$idUser2,$idobjet1,$idobjet2);
+        $query = $this->db->query($sql);
+        $result = $query->affected_row();
     }
 }
 ?>
